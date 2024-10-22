0960dfb77977d3f13d344b9dcf87d090a63043c2
Santiago Figun <santi.figun@gmail.com>
2024-10-22 13:42:36 -0300

    verCuidador

--- src/main/java/com/pandateam/sfsaludmaven/backend/mappers/CuidadorMapper.java
+++ src/main/java/com/pandateam/sfsaludmaven/backend/mappers/CuidadorMapper.java
@@ -8,22 +8,30 @@ import com.pandateam.sfsaludmaven.backend.dto.CuidadorDTO;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 /**
  *
  * @author santi
  */
-public class CuidadorMapper{
-    
+public class CuidadorMapper {
+
     public CuidadorDTO map(ResultSet rs) throws SQLException {
         CuidadorDTO cuidadorDTO = new CuidadorDTO();
-        
-        cuidadorDTO.setIdCuidador(rs.getInt("Per_ID"));
-        cuidadorDTO.setProfesion(rs.getString("C_Profesion"));
-        cuidadorDTO.setExperiencia(rs.getString("C_Experiencia"));
-        cuidadorDTO.setCategoria(rs.getString("C_Categoria"));
-        
+
+        if (rs.next()) {
+            cuidadorDTO.setIdCuidador(rs.getInt("Per_ID"));
+            cuidadorDTO.setProfesion(rs.getString("C_Profesion"));
+            cuidadorDTO.setExperiencia(rs.getString("C_Experiencia"));
+            cuidadorDTO.setCategoria(rs.getString("C_Categoria"));
+            cuidadorDTO.setNombre(rs.getString("Per_Nombre"));
+            cuidadorDTO.setApellido(rs.getString("Per_Apellido"));
+            cuidadorDTO.setDni(rs.getString("Per_NumeroDocumento"));
+            cuidadorDTO.setFechaNacimiento(rs.getDate("Per_FechaNacimiento"));
+            cuidadorDTO.setTelefono(rs.getInt("Per_Telefono"));
+            cuidadorDTO.setMail(rs.getString("Per_Correo"));
+            cuidadorDTO.setEspPersona(rs.getString("Per_Esp"));
+        }
+
         return cuidadorDTO;
     }
-    
+
 }
